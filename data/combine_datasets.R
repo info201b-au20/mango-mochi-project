daeb9148d56a09ddd2c7f42a7cecbf2d1beb7655
ZIYUAN CHEN <czy98765@gmail.com>
2020-11-02 01:29:24 -0800

    made a file of the codes to download the datasets into RStudio, have codes for the first 10 files in the folder (alphabetically).

---
+++ data/combine_datasets.R
@@ -0,0 +1,38 @@
+# Labor Force Statistics from the Current Population Survey dataset 
+# Sent session to current directory, which is the data folder 
+
+# First 10 datasets 
+Alternative_measure_of_labor_underutilization <- 
+  read_excel("Labor Force Statistics from the Current Population Survey, 40 matrixes/Alternative measure of labor underutilization U-6 - LNS13327709.xlsx")
+
+Average_Weeks_Unemployed <- 
+  read_excel("Labor Force Statistics from the Current Population Survey, 40 matrixes/Average Weeks Unemployed - LNS13008275.xlsx")
+
+Civilian_Labor_Force_Level <- 
+  read_excel("Labor Force Statistics from the Current Population Survey, 40 matrixes/Civilian Labor Force Level - LNS11000000.xlsx")
+
+Civilian_Labor_Force_Participation_Rate <- 
+  read_excel("Labor Force Statistics from the Current Population Survey, 40 matrixes/Civilian Labor Force Participation Rate - LNS11300000.xlsx")
+
+Discouraged_Workers <- 
+  read_excel("Labor Force Statistics from the Current Population Survey, 40 matrixes/Discouraged Workers - LNS15026645.xlsx")
+
+Employed_Usually_Work_Full_Time <- 
+  read_excel("Labor Force Statistics from the Current Population Survey, 40 matrixes/Employed, Usually Work Full Time - LNS12500000.xlsx")
+
+Employed_Usually_Work_Part_Time <- 
+  read_excel("Labor Force Statistics from the Current Population Survey, 40 matrixes/Employed, Usually Work Part Time - LNS12600000.xlsx")
+
+Employment_Level <- 
+  read_excel("Labor Force Statistics from the Current Population Survey, 40 matrixes/Employment Level - LNS12000000.xlsx")
+
+Employment_Level_Nonag_Industries_At_Work_1_34_Hrs_Usually_Work_Full_time_Bad_Weather <- 
+  read_excel("Labor Force Statistics from the Current Population Survey, 40 matrixes/Employment Level, Nonag. Industries, At Work 1-34 Hrs, Usually Work Full time, Bad Weather - LNU02033224.xlsx")
+
+Employment_Level_Nonag_Industries_With_a_Job_not_at_Work_Bad_Weather <- 
+  read_excel("Labor Force Statistics from the Current Population Survey, 40 matrixes/Employment Level, Nonag. Industries, With a Job not at Work, Bad Weather - LNU02036012.xlsx")
+
+# Second 10 datasets 
+
+
+
