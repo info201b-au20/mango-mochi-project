d78a965e7b17b6c3a00edc89ddcb400947289727
ZIYUAN CHEN <czy98765@gmail.com>
2020-11-13 07:33:46 -0800

    made a new .csv with data from 2003-2020 (correct, & without NA's), edited and finished up summary and table summary

--- scripts/table_summary.R
+++ scripts/table_summary.R
@@ -1,11 +1,16 @@
-#  creates a table of summary information to be included
-# load("~/Desktop/info_201/mango-mochi-project/data/Unemployment.RData")
+# creates a table of summary information to be included
 
 library(tidyverse)
 
-unemployment <- read.csv("data/unemployment.csv")
+# unemployment <- read.csv("data/unemployment.csv")
+
+unemployment<- read.csv("unemployment_2003-2020.csv")
+View(unemployment)
+
+########################Table 1, max values#####################################
+
 # A table of the maximum unemployment rate of each feature by year.
-Unemployment_summary <- unemployment %>% drop_na()
+Unemployment_summary <- unemployment # %>% drop_na(), shouldn't be needed
 Unemployment_summary <- unemployment %>% 
   group_by(Year) %>% 
   summarize(
@@ -22,6 +27,35 @@ Unemployment_summary <- unemployment %>%
             ) %>% 
   arrange(desc(Year))
 
+View(Unemployment_summary)
 
+
+
+########################Table 2, mean values####################################
+
+# A table of the mean unemployment rate of each feature by year.
+Unemployment_summary_mean <- unemployment 
+Unemployment_summary_mean <- unemployment %>% 
+  group_by(Year) %>% 
+  summarize(
+    Men = round(mean(Unemployment_Rate_20_Years_Over_Men), 1), 
+    Women = round(mean(Unemployment_Rate_20_Years_Over_Women), 1),
+    Bachelor_Degree_or_Higher = round(mean(Unemployment_Rate_25_Years_Over_Bachelor_s_Degree_and_Higher), 1),
+    High_School_Grad = round(mean(Unemployment_Rate_25_Years_Over_High_School_Graduates_No_College), 1), 
+    Some_High_School = round(mean(Unemployment_Rate_25_Years_Over_Less_than_a_High_School_Diploma), 1), 
+    Some_College_or_Associate_Degree = round(mean(Unemployment_Rate_25_Years_Over_Some_College_or_Associate_Degree), 1), 
+    Asian = round(mean(Unemployment_Rate_Asian), 1), 
+    Black_or_African_American = round(mean(Unemployment_Rate_Black_or_African_American), 1), 
+    Hispanic_or_Latino = round(mean(Unemployment_Rate_Hispanic_or_Latino), 1), 
+    White = round(mean(Unemployment_Rate_White), 1)
+  ) %>% 
+  arrange(desc(Year))
+
+
+
+
+# For the purpose of debugging, testing individual lines 
+# unemployment_test <- unemployment %>% 
+  
 
 
